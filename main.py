e81a9f4ede97201e257e6ac381c24d5b64a82edf
Kainan H. <kainanhenriq18@hotmail.com>
2025-05-25 17:16:24 -0300

    Adicionado env.json e leitor de env.json

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for
+from uuid import UUID
+import json
+
+# Carregar o arquivo JSON
+with open('env.json') as f:
+    env = json.load(f)
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
