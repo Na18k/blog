37dd11161b0f7958337becfbd0ba54df9005796f
Kainan H. <kainanhenriq18@hotmail.com>
2025-05-25 19:05:00 -0300

    Adicionado sistema de login

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 from uuid import UUID
 import json
+from functools import wraps
 
 # Carregar o arquivo JSON
 with open('env.json') as f:
@@ -8,3 +9,12 @@ with open('env.json') as f:
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = env["system"]["secretKey"]
+
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not session.get("is_logged"):
+            return redirect(url_for('login', next=request.url))
+        return f(*args, **kwargs)
+    return decorated_function
