d7b41ff614875bdbfebfde107debd493fa6d9b65
Kainan H. <kainanhenriq18@hotmail.com>
2025-05-25 19:44:32 -0300

    Adicionado rota de login

--- routes.py
+++ routes.py
@@ -3,3 +3,11 @@ from main import *
 @app.route('/')
 def index():
     return render_template('list_post.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        senha = request.form.get('password')
+
+    return render_template('auth/login.html')
